4c270af657e8562ff0e4b4765818f6bc3117bcf2
Krys Tyler <krys.tyler@snowflake.com>
2024-03-15 08:23:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import streamlit as st
 
 st.header('Are You Snow-A-Mazing?')
 st.write('You\'re here to introduce yourself so that we\'ll know who to send your badge to, once you\'ve shown us that you are, in fact, Snow-A-Mazing!')
+step = 1
 
 st.write('Begin by telling us who sent you...')
 referred_by = st.selectbox('Who sent you to this app?', ('<choose one>','DORA', 'BUILD Event Moderator', 'Josh'))
 time_for_step_2 = st.button ('Submit Referrer')
 
 if time_for_step_2:
-  if referred_by == '<choose one>':
+  step = 2
+  if referred_by == '<choose one>' and step ==2:
     st.write('Dude? Seriously? You were supposed to choose a value from the list before you clicked the button.')
-  elif referred_by == 'DORA':
+  elif referred_by == 'DORA' and step ==2::
     st.write('Yay DORA!')
     workshop = st.selectbox('Which workshop/badge are you currently working on?', ('Badge 1: DWW', 'Badge 2: CMCW', 'Badge 3: DABW', 'Badge 4: DLKW', 'Badge 5: DNGW'))
     step_3_dora = st.button ('Submit Workshop Name')
