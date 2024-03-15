543fadff53d08b02cd3e23ed10a86928c2cbe80d
Krys Tyler <krys.tyler@snowflake.com>
2024-03-14 18:00:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ st.write('We think you are! Someone likely sent you to this page to register for
 st.write("Begin by telling us who sent you...")
 referred_by = st.selectbox("Who sent you to this app?", ("DORA", "BUILD Event Moderator", "Josh")
                                                     
-if referred_by = "BUILD Event Moderator":
+if referred_by == "BUILD Event Moderator":
   my_event = st.selectbox("What event are you attending?", ("Warsaw -  25 Mar 2024", "Bay Area - 11 Apr 2024","Dallas - 25 Apr 2024"))
-else if referred_by = "DORA":
+else if referred_by == "DORA":
   my_workshop = st.selectbox("Which workshop/badge are you working on?", ("Badge 1: DWW", "Badge 2: CMCW", "Badge 3: DABW", "Badge 4: DLKW", "Badge 5:DNGW"))
   
              
