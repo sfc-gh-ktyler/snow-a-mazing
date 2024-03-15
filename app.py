d98c80d3271e741d57c5ad58ec98fdc3465e6d58
Krys Tyler <krys.tyler@snowflake.com>
2024-03-14 18:03:45 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.header('Are You Snow-A-Mazing?')
 st.write('We think you are! Someone likely sent you to this page to register for a BADGE. Maybe you came from learn.snowflake.com? Or from a Snowflake BUILD event? In any case, you\'re here to introduce yourself so that we\'ll know who to send your badge to, once you\'ve shown us that you are, in fact, Snow-A-Mazing!')
 
 st.write("Begin by telling us who sent you...")
-referred_by = st.selectbox("Who sent you to this app?", ("DORA", "BUILD Event Moderator", "Josh")
+referred_by = st.selectbox("Who sent you to this app?", ("DORA", "BUILD Event Moderator", "Josh"))
 st.write(referred_by)
                            
 st.stop()
