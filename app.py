9fc56d96af6c6f705132f2677ed0cd2e44c228fd
Krys Tyler <krys.tyler@snowflake.com>
2024-03-14 17:45:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.header('Are You Snow-A-Mazing?')
-
+st.write('We think you are! Someone likely sent you to this page to register for a BADGE. Maybe you came from learn.snowflake.com? Or from a Snowflake BUILD event? In any case, you're here to introduce yourself so that we'll know who to send your badge to, once you've shown us that you are, in fact, Snow-A-Mazing!')
