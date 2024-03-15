95c9f97cd0846328dc0d104e5e5f6f00c033cbc5
Krys Tyler <krys.tyler@snowflake.com>
2024-03-15 08:38:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.header('Are You Snow-A-Mazing?')
 st.write('You\'re here to introduce yourself so that we\'ll know who to send your badge to, once you\'ve shown us that you are, in fact, Snow-A-Mazing!')
 
 badge_options = {"DORA": ['Badge 1: DWW', 'Badge 2: CMCW', 'Badge 3: DABW', 'Badge 4: DLKW', 'Badge 5: DNGW'], 
-                "BUILD": ['Warsaw: 25 Mar 2024', 'Bay Area: 11 Apr 2024', 'Dallas: 25 Apr 2024']
+                "BUILD": ['Warsaw: 25 Mar 2024', 'Bay Area: 11 Apr 2024', 'Dallas: 25 Apr 2024'],
                  "SE": ['College 1', 'College 2', 'College 3']
                 }
 
