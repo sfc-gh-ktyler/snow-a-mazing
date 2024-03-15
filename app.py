ab298ddb4db48cbb9e3d743f03ab110f6c7342c2
Krys Tyler <krys.tyler@snowflake.com>
2024-03-14 18:33:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.write('You\'re here to introduce yourself so that we\'ll know who to send you
 
 st.write('Begin by telling us who sent you...')
 referred_by = st.selectbox('Who sent you to this app?', ('<choose one>','DORA', 'BUILD Event Moderator', 'Josh'))
-time_for_step_2 = st.button ('Submit')
+time_for_step_2 = st.button ('Submit Referrer')
 
 if time_for_step_2:
   if referred_by == '<choose one>':
@@ -13,16 +13,16 @@ if time_for_step_2:
   elif referred_by == 'DORA':
     st.write('Yay DORA!')
     workshop = st.selectbox('Which workshop/badge are you currently working on?', ('Badge 1: DWW', 'Badge 2: CMCW', 'Badge 3: DABW', 'Badge 4: DLKW', 'Badge 5: DNGW'))
-    step_3_dora = st.button ('Submit')
+    step_3_dora = st.button ('Submit Workshop Name')
     st.write('DORA Says: I love that workshop!' + workshop + 'is my FAVORITE of the Hands On Essentials Workshops!!')
   elif referred_by == 'BUILD Event Moderator':
     st.write('Yay BUILD!')
     build_local = st.selectbox('Which event are you currently attending?', ('Warsaw: 25 Mar 2024', 'Bay Area: 11 Apr 2024', 'Dallas: 25 Apr 2024'))
-    step_3_build = st.button ('Submit')
+    step_3_build = st.button ('Submit Build Event')
   else:
     st.write('Yay Josh!')
     build_local = st.selectbox('Which SE College Course are you currently working on?', ('College A', 'College B', 'College C'))
-    step_3_se_college = st.button ('Submit')
+    step_3_se_college = st.button ('Submit SE College')
 
 
   
