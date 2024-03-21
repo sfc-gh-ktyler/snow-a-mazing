18b2e3f70d49f570cec9de6f5a7ac439bfc0f694
Krys Tyler <krys.tyler@snowflake.com>
2024-03-21 09:40:49 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,19 @@ import pandas as pd
 st.header('Are You Snow-A-Mazing?')
 st.write('You\'re here to introduce yourself so that we\'ll know who to send your badge to, once you\'ve shown us that you are, in fact, Snow-A-Mazing!')
 
-email = st.text_input('Enter your email address:')
+st.write("Your UNI ID can be found in the top-right corner of the workshop page. It begins with 053 and is a string of letters and numbers.")
+uni_id = st.text_input('Enter your learn.snowflake.com UNI ID:')
+
+st.write(uni_id[:3])
+
+if uni_id[:3] = '005':
+  st.write = 'So far, so good'
+
+
+  
+  
+
+
 
 badge_options = ('Badge 1: DWW', 'Badge 2: CMCW', 'Badge 3: DABW', 'Badge 4: DLKW', 'Badge 5: DNGW')
 
