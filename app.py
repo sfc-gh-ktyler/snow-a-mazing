48cef839441f234111c117ee808b64088347605b
Krys Tyler <krys.tyler@snowflake.com>
2024-03-22 11:52:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 import streamlit_authenticator as stauth
 import pandas as pd
+import 
 
 st.header('Are You Snow-A-Mazing?')
-st.write('You\'re here to introduce yourself so that we\'ll know who to send your badge to, once you\'ve shown us that you are, in fact, Snow-A-Mazing!')
+st.write('Welcome to the learn.snowflake.com Workshop Badge Management app!')
 
-st.write("Your UNI ID can be found in the top-right corner of the workshop page. It begins with 053 and is a string of letters and numbers.")
 uni_id = st.text_input('Enter your learn.snowflake.com UNI ID:')
+uni_email = st.text_input('Enter your learn.snowflake.com EMAIL Address:')
+
+st.write("TIPS:")
+st.write("Your UNI ID can be found in the top-right corner of the workshop page. It begins with 053 and is a string of letters and numbers.")
+st.write("Your learn.snowflake.com UNI_ID and EMAIL can be found at https://learn.snowflake.com/account/settings")
+st.write("-----")
 
 starts_right = uni_id[:3]
 
