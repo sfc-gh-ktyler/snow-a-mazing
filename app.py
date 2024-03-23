e014991c06e3a16673c425037962d59fc2505e63
Krys Tyler <krys.tyler@snowflake.com>
2024-03-23 07:03:32 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,16 +26,39 @@ if find_my_uni_record:
     if user_rows>=1:
         st.dataframe(this_user_df)
     else:
-        st.write("There is no record of the UNI_ID/EMAIL combination you entered. Please double-check the info you entered, read the tips below, and try again") 
+        st.write("There is no record of the UNI_ID/UUID combination you entered. Please double-check the info you entered, read the tips below, and try again") 
 
 st.write("-----")
 st.subheader("TIPS:")
 st.write("Your UNI ID can be found in the top-right corner of the workshop page. It begins with 053 and is a string of letters and numbers.")
-st.write("Your learn.snowflake.com UNI_ID and EMAIL can be found at https://learn.snowflake.com/account/settings")
+st.write("Your learn.snowflake.com UNI_UUID is displayed on the page that linked you to this app.")
 st.write("-----")
 
+st.subheader("Your Badge: Your Name & Email")
+st.write("We need your name for your badge. We want it to look nice. We have rules. Please follow them."
 
-st.write("You're too legit to quit!")
+ with st.form("badge_name_and_email"):
+        st.write("Confirm Your Name and Email for Any Badges That Might Be Issued")     
+        firstname = st.text_input("First Name")
+        middlename = st.text_input('Middle Name (Optional)')
+        lastname = st.text_input('Last Name')
+        email = st.selectbox('Badge Offered:',('BWDE','BWCX','BWGA','BWNA'))
+       
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Update Badge Details")
+        if submitted:
+            session.call('dr_build_courses.regs.ADD_EVENT_SP',series, start_date, city, country, days_to_complete, badge )
+            st.success('Event Added', icon='🚀')
+            st.experimental_rerun()
+    st.markdown("""---""")         
+
+
+    
+
+         
+         
+
 
 starts_right = uni_id[:3]
 
