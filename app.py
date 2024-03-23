163369f34b97cade7bde5002ca771cd967167289
Krys Tyler <krys.tyler@snowflake.com>
2024-03-23 09:16:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 
 cnx=st.connection("snowflake")
-
+# Tabs
+tab1, tab2 = st.tabs(["Edit Badge Info", "Finding Info", "Name Entry Rules"])
+                                              
 # Load the table as a dataframe using the Snowpark Session.
 @st.cache_data
 def load_uni_users():
@@ -19,42 +21,67 @@ uni_uuid = st.text_input('Enter the secret UUID displayed on the DORA is Listeni
 find_my_uni_record = st.button("Find my UNI User Info")
 
 if find_my_uni_record:
-    #this_user_df =  uni_users_df.query('UNI_ID=="005VI0000052bmzYAA" & =="femke.van.verseveld@inergy.nl" ')
     this_user_df =  uni_users_df.query('UNI_ID=="' + uni_id + '" & UNI_UUID=="'+ uni_uuid +'" ')
     user_rows = this_user_df.shape[0]
     
     if user_rows>=1:
         st.dataframe(this_user_df)
     else:
-        st.write("There is no record of the UNI_ID/UUID combination you entered. Please double-check the info you entered, read the tips below, and try again") 
+        st.write("There is no record of the UNI_ID/UUID combination you entered. Please double-check the info you entered, read the tips on the FINDING INFO tab, and try again") 
 
-st.write("-----")
-st.subheader("TIPS:")
-st.write("Your UNI ID can be found in the top-right corner of the workshop page. It begins with 053 and is a string of letters and numbers.")
-st.write("Your learn.snowflake.com UNI_UUID is displayed on the page that linked you to this app.")
-st.write("-----")
+with tab1:
+    st.subheader("Your Badge: Your Name & Email")
+    st.write("We need your name for your badge. We want it to look nice. We have rules. Please follow them.")
 
-st.subheader("Your Badge: Your Name & Email")
-st.write("We need your name for your badge. We want it to look nice. We have rules. Please follow them.")
+    with st.form("badge_name_and_email"):
+        st.write("Confirm Your Name and Email for Any Badges That Might Be Issued")     
+        givenname = st.text_input("Given Name")
+        middlename = st.text_input('Middle Name (Optional)')
+        familyname = st.text_input('Family Name')
+        cultural_order = st.checkbox('My culture displays my Family Name first. e.g. Japanese, Korean, Chinese')
+        email = st.text_input('Email Address I want associated with my badge:')
 
-with st.form("badge_name_and_email"):
-    st.write("Confirm Your Name and Email for Any Badges That Might Be Issued")     
-    firstname = st.text_input("First Name")
-    middlename = st.text_input('Middle Name (Optional)')
-    lastname = st.text_input('Last Name')
-    email = st.selectbox('Badge Offered:',('BWDE','BWCX','BWGA','BWNA'))
-
-    submitted = st.form_submit_button("Update Badge Details")
-    if submitted:
+        submitted = st.form_submit_button("Update Badge Details")
+        if submitted:
+            
             session.call('amazing.app.UPDATE_BADGE_INFO_SP',firstname, middlename, lastname, email )
             st.success('Badge Info Updates', icon='🚀')
             st.experimental_rerun()
-    st.markdown("""---""")         
+        st.markdown("""---""")         
 
+with tab2:
+    st.write("-----")
+    st.subheader("FINDING YOUR INFORMATION:")
+    st.write("In order to make edits, you must enter the correct combination of your UNI ID and the UUID we have assigned to you.")
+    st.write("Your UNI ID can be found in the top-right corner of the workshop page. It begins with 053 and is a string of letters and numbers.")
+    st.write("Your learn.snowflake.com UNI_UUID is displayed on the page of the workshop that linked you to this app.")
+st.write("-----")
 
-    
+with tab3:
+    st.subheader("Our Very Nit-picky Name Rules")
+    st.write("We want your name to look nice on your badge(s). We want your badge(s) to accurately represent your name and securely represent your accomplishments."
+    st.write("Because of this, we have rules around what names can and should be used. If your entries fail, read over these rules until you figure out what went wrong."
+    st.write("-----")
 
-         
+    st.write("CHARACTER CASE RULES:")
+    st.write("* Proper Mixed Case is required for given names and middle names. (Initial-letter capitalization)")
+    st.write("* ALL UPPER CASE is allowed for family names.")
+    st.write("* All lower case is allowed for in family names for nobiliary particles like van, van der, de, de la, etc. but the other parts of the family name should be mixed case."
+
+    st.write("NAME LENGTH RULES:")
+    st.write("We recognize that East Asian names can sometimes be very few characters. Southeast Asian and Spanish names can sometimes be many, many characters.")
+    st.write("We do not like the recent Southeast Asian trend of using a few initials as a last name. e.g. Kumar KS is not a good badge name because there are too many Kumar KS\'s in the world.")
+    st.write("If you are Southeast Asian, please use the longer version of your name - not a given name followed by initials for the family names.")
+
+             
+             
+             
+
+             
+            
+             
+             
+
          
 
 
