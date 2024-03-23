6ff3fd9a1694a6e4a33fa851ef5e97cd9432cdac
Krys Tyler <krys.tyler@snowflake.com>
2024-03-23 07:14:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -37,19 +37,17 @@ st.write("-----")
 st.subheader("Your Badge: Your Name & Email")
 st.write("We need your name for your badge. We want it to look nice. We have rules. Please follow them."
 
- with st.form("badge_name_and_email"):
-        st.write("Confirm Your Name and Email for Any Badges That Might Be Issued")     
-        firstname = st.text_input("First Name")
-        middlename = st.text_input('Middle Name (Optional)')
-        lastname = st.text_input('Last Name')
-        email = st.selectbox('Badge Offered:',('BWDE','BWCX','BWGA','BWNA'))
-       
+with st.form("badge_name_and_email"):
+    st.write("Confirm Your Name and Email for Any Badges That Might Be Issued")     
+    firstname = st.text_input("First Name")
+    middlename = st.text_input('Middle Name (Optional)')
+    lastname = st.text_input('Last Name')
+    email = st.selectbox('Badge Offered:',('BWDE','BWCX','BWGA','BWNA'))
 
-        # Every form must have a submit button.
-        submitted = st.form_submit_button("Update Badge Details")
-        if submitted:
-            session.call('dr_build_courses.regs.ADD_EVENT_SP',series, start_date, city, country, days_to_complete, badge )
-            st.success('Event Added', icon='🚀')
+    submitted = st.form_submit_button("Update Badge Details")
+    if submitted:
+            session.call('amazing.app.UPDATE_BADGE_INFO_SP',firstname, middlename, lastname, email )
+            st.success('Badge Info Updates', icon='🚀')
             st.experimental_rerun()
     st.markdown("""---""")         
 
