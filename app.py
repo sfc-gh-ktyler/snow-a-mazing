4e18aab1842417c7d9600bbf86161669535a9a8d
Krys Tyler <krys.tyler@snowflake.com>
2024-03-25 13:07:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ with tab2:
         if submit_edits:
             session.call('AMAZING.APP.UPDATE_BADGENAME_BADGEEMAIL_SP',uni_id, uni_uuid, edited_given, edited_middle, edited_family, edited_email)
             st.success('Badge Info Updates', icon='🚀')
-            st.experimental_rerun()
+            st.rerun()
             st.markdown("""---""")  
         
             with st.form("display_formatting"):
