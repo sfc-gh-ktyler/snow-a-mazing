2037cbd939556c3963feaef274248bf2c2904de6
Krys Tyler <krys.tyler@snowflake.com>
2024-03-25 18:01:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ with tab4:
             if 'workshop' not in st.session_state:
                 st.session_state['workshop'] = "AWARD-" + badge_options[9:13] #get characters 9 through 13 which should be the acronym
                 st.write(st.session_state.workshop)
-                acct_info_sql = trials_results.filter(col("award_id")==st.session_state.workshop
+                acct_info_sql = trials_results.filter(col("award_id")==st.session_state.workshop)
                 st.write(acct_info_sql)
             
             workshop_chosen = st.form_submit_button("Show Data on My Chosen Workshop")
