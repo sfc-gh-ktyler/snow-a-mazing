e4376c4d9c5a5b3b67809c0e2fd15ab3e88121a4
Krys Tyler <krys.tyler@snowflake.com>
2024-03-26 06:13:48 -0700

    Update app.py

--- app.py
+++ app.py
@@ -143,9 +143,9 @@ with tab4:
             st.dataframe(badge_options)
             workshop = st.selectbox("Choose Workshop/Badge want to enter/edit account info for:", options=badge_options, key=1)
             st.session_state['workshop_chosen'] = workshop[9:13]
-            st.write('AWARD-'+st.session_state.workshop_chosen)
-            #acct_info_sql = trials_results.filter(col("award_id")==st.session_state.workshop_chosen)
-            # st.write(acct_info_sql)
+            award_name = 'AWARD-'+st.session_state.workshop_chosen
+            acct_info_sql = trials_results.filter(col("award_id")==award_name)
+            st.write(acct_info_sql)
             workshop_chosen = st.form_submit_button("Show Data on My Chosen Workshop")
  
 
